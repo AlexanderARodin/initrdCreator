a9a0eed1b50649ff0e1ffe06131d8630ce440a36
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-05 05:11:39 +0300

    Update reload-self.sh

--- scripts/reload-self.sh
+++ scripts/reload-self.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+SRCPWD=$PWD
+
 echo 're-cloning..'
 cd ..
 rm -Rf ./initrdCreator
 git clone https://github.com/AlexanderARodin/initrdCreator.git
-cd ..; cd -
-cd ./initrdCreator
+#cd ..; cd -
+#cd ./initrdCreator
+echo 
+pwd
+cd $SRCPWD
+pwd
 chmod +x ./scripts/*
 
 echo '..probably OK'
