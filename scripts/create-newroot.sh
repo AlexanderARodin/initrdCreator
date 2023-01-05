0f538738238a32a0c97ca8f87a11ba02bb18912c
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-05 05:26:33 +0300

    Update create-newroot.sh

--- scripts/create-newroot.sh
+++ scripts/create-newroot.sh
@@ -4,4 +4,23 @@ set -e
 
 source .config
 
+# new root
 sudo mkdir -v $NEWROOT
+
+
+# system dirs
+sudo mkdir -v $NEWROOT/dev
+sudo mkdir -v $NEWROOT/proc
+sudo mkdir -v $NEWROOT/sys
+
+
+# software subdirs
+sudo mkdir -v $NEWROOT/usr
+sudo mkdir -v $NEWROOT/usr/bin
+sudo ln -sv ./usr/bin $NEWROOT)/bin
+
+sudo mkdir -v $NEWROOT/usr/sbin
+sudo ln -sv ./usr/sbin $NEWROOT/sbin
+
+sudo mkdir -v $NEWROOT/usr/lib
+sudo ln -sv ./usr/lib $NEWROOT/lib
