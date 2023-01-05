e855319d3ffa0ba36741ba8a7a63513a1f3741ec
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-05 16:08:28 +0300

    Update run-init-chroot.sh

--- scripts/run-init-chroot.sh
+++ scripts/run-init-chroot.sh
@@ -4,3 +4,9 @@ source .config
 
 echo "run init into new root: $NEWROOT"
 sudo chroot $NEWROOT /init
+
+# unmount pseudo-system
+sudo umount $NEWROOT/sys
+sudo umount $NEWROOT/proc
+sudo umount $NEWROOT/dev
+
