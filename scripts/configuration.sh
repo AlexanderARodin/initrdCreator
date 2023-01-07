535ccc0e3f847b7eee3cc982a47bf8aab3f8383f
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-08 01:58:40 +0300

    Update configuration.sh

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -9,3 +9,6 @@ sudo cp -rv ./newroot/* $NEWROOT
 if [ -f "$NEWROOT/init" ]; then
 	sudo chmod +x "$NEWROOT/init"
 fi
+if [ -f "$NEWROOT/etc/init.d/rcS" ]; then
+	sudo chmod +x "$NEWROOT/etc/init.d/rcS"
+fi
