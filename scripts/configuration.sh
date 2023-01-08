aad603b3dfd8f72082e08e04c4691d3c6b94aee3
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-08 22:51:51 +0300

    Update configuration.sh

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -9,6 +9,11 @@ sudo cp -rv ./newroot/* "$NEWROOT"
 if [ -f "$NEWROOT/init" ]; then
 	sudo chmod +x "$NEWROOT/init"
 fi
+if [ -f "$NEWROOT/deployment.sh" ]; then
+	sudo chmod +x "$NEWROOT/deployment.sh"
+fi
+
+
 
 #sudo cp -v "$TMUX" "$NEWROOT/bin"
 #sudo chmod +x "$NEWROOT/bin/tmux"
