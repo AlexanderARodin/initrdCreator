f82ef3910134f9945c9db2965ffea560cb36f953
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-08 17:49:50 +0300

    Update run-chroot.sh

--- scripts/run-chroot.sh
+++ scripts/run-chroot.sh
@@ -7,7 +7,10 @@ source .config
 echo "run pseudo-init in new root: $NEWROOT"
 
 sudo cp -v ./scripts/supporting/pseudo-init $NEWROOT
+
+sudo chmod +x $NEWROOT/pseudo-init
 sudo chroot $NEWROOT /pseudo-init
+
 sudo rm -vf $NEWROOT/pseudo-init
 
 #sudo chroot $NEWROOT /bin/sh
