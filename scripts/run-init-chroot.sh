251dc9deecab268b807c10e4c870ee4f938f3aa4
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-08 18:12:57 +0300

    Update run-init-chroot.sh

--- scripts/run-init-chroot.sh
+++ scripts/run-init-chroot.sh
@@ -8,10 +8,10 @@ sudo chroot $NEWROOT /init $1
 
 # unmount pseudo-system
 sudo umount $NEWROOT/dev/pts
-sudo rm -Rvf $NEWROOT/dev/pts
 sudo umount $NEWROOT/dev
-sudo rm -Rvf $NEWROOT/dev
 sudo umount $NEWROOT/proc
-sudo rm -Rvf $NEWROOT/proc
 sudo umount $NEWROOT/sys
+
+sudo rm -Rvf $NEWROOT/dev
+sudo rm -Rvf $NEWROOT/proc
 sudo rm -Rvf $NEWROOT/sys
