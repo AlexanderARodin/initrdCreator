240d33e5e562f96a979dac776cbdbad89f14db2e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-10 01:33:32 +0300

    Update create-newroot.sh

--- scripts/create-newroot.sh
+++ scripts/create-newroot.sh
@@ -24,3 +24,13 @@ sudo ln -sv ./usr/sbin $NEWROOT/sbin
 
 sudo mkdir -v $NEWROOT/usr/lib
 sudo ln -sv ./usr/lib $NEWROOT/lib
+
+# config subdirs
+sudo mkdir -v $NEWROOT/etc
+
+# home dir
+sudo mkdir -v $NEWROOT/home
+
+# var subdirs
+sudo mkdir -v $NEWROOT/var
+sudo mkdir -v $NEWROOT/var/service
