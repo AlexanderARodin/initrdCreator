19082e91cf0805dec143ede8bebc2ae300c46d0e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-10 16:20:53 +0300

    Update 40-configuration.sh

--- scripts/40-configuration.sh
+++ scripts/40-configuration.sh
@@ -10,7 +10,7 @@ if [ -f "$NEWROOT/init" ]; then
 	sudo chmod -v +x "$NEWROOT/init"
 fi
 
-run-parts -v ---
+#run-parts -v ---
 
 if [ -f "$NEWROOT/deployment.sh" ]; then
 	sudo chmod -v +x "$NEWROOT/deployment.sh"
