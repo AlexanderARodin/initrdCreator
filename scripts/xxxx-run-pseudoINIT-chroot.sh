7724b261f645364784f7756b9b9cfa2a63788a2f
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-10 21:41:18 +0300

    Update and rename ri-run-init-chroot.sh to xxxx-run-pseudoINIT-chroot.sh

--- scripts/xxxx-run-pseudoINIT-chroot.sh
+++ scripts/xxxx-run-pseudoINIT-chroot.sh
@@ -3,9 +3,12 @@
 source .config
 source ./scripts/.lib
 
-echo "run init into new root: $NEWROOT"
+# unmount & remove pseudo-system
+BindSystemVFS
+
+echo "run pseudoINIT into new root: $NEWROOT"
 echo "whith param: $1"
-sudo chroot $NEWROOT /init $1
+sudo chroot $NEWROOT $1
 
 # unmount & remove pseudo-system
 RemoveSystemVFS
