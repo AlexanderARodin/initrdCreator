002e1605bd9ff4223451a05a3fbd403edbee0b36
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-11 00:58:00 +0300

    Update and rename xxxx-chroot-run-script.sh to ----xxxx-chroot-run-script.sh

--- scripts/----xxxx-chroot-run-script.sh
+++ scripts/----xxxx-chroot-run-script.sh
@@ -2,7 +2,7 @@
 
 source .config
 
-echo "run SH in new root: $NEWROOT"
+echo "run SCRIPT in new root: $NEWROOT"
 
 sudo cp -v ./scripts/chrooters/SCRIPT "$NEWROOT"
 sudo chmod -v +x "$NEWROOT/SCRIPT"
