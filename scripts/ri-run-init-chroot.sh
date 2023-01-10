0828ab56f5d1d2991a285d2eaec6b1f71b27aa06
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-10 17:01:12 +0300

    Update ri-run-init-chroot.sh

--- scripts/ri-run-init-chroot.sh
+++ scripts/ri-run-init-chroot.sh
@@ -1,17 +1,12 @@
 #!/bin/env bash
 
 source .config
+source ./scripts/.lib
 
 echo "run init into new root: $NEWROOT"
 echo "whith param: $1"
 sudo chroot $NEWROOT /init $1
 
-# unmount pseudo-system
-sudo umount $NEWROOT/dev/pts
-sudo umount $NEWROOT/dev
-sudo umount $NEWROOT/proc
-sudo umount $NEWROOT/sys
+# unmount & remove pseudo-system
+RemoveSystemVFS
 
-sudo rm -Rvf $NEWROOT/dev
-sudo rm -Rvf $NEWROOT/proc
-sudo rm -Rvf $NEWROOT/sys
