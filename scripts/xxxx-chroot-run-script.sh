c8bb1ef261f021d2e66144ac739e31aa03a5183e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-11 00:38:01 +0300

    Update xxxx-chroot-run-script.sh

--- scripts/xxxx-chroot-run-script.sh
+++ scripts/xxxx-chroot-run-script.sh
@@ -3,4 +3,10 @@
 source .config
 
 echo "run SH in new root: $NEWROOT"
-sudo chroot $NEWROOT /SCRIPT $1
+
+sudo cp -v ./scripts/chrooters/SCRIPT "$NEWROOT"
+sudo chmod -v +x "$NEWROOT/SCRIPT"
+
+	sudo chroot $NEWROOT /SCRIPT $1
+
+sudo rm -fv "$NEWROOT/SCRIPT"
