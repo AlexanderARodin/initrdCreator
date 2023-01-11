91c9b58d83551a2827877738ced221d7fad47d0d
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-11 15:20:28 +0300

    Update raa-tty.sh

--- components/DEBUGGING/DBG/raa-tty.sh
+++ components/DEBUGGING/DBG/raa-tty.sh
@@ -2,11 +2,15 @@
 
 TTY="tty$1"
 DISP="<> /dev/$TTY >&0 2>&1"
-MSG="It's $TTY"
+MSG_IN="DEBUGGING $TTY - intro"
+MSG_OUT="DEBUGGING $TTY - outro"
 
 exec 2>&1
 
-setsid /bin/sh -c "clear $DISP"
-setsid /bin/sh -c "echo $MSG $DISP"
+#setsid /bin/sh -c "clear $DISP"
+setsid /bin/sh -c "echo $MSG_IN $DISP"
 setsid /bin/sh -c "exec login $DISP"
-setsid /bin/sh -c "clear $DISP"
+setsid /bin/sh -c "echo $DISP"
+setsid /bin/sh -c "echo $MSG_OUT $DISP"
+setsid /bin/sh -c "echo $DISP"
+#setsid /bin/sh -c "clear $DISP"
