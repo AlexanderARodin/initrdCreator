838e25fcf24fe5edfcc62f3b1ec5cd802b9cb09f
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-11 04:35:17 +0300

    Update raa-tty.sh

--- components/DEBUGGING/DBG/raa-tty.sh
+++ components/DEBUGGING/DBG/raa-tty.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
+set -x
 
+TTY=$1
+DISP="<> /dev/tty$TTY >&0 2>&1"
+
+exec 2>&1
+echo "getty-$TTY!!.."
+
+setsid /bin/sh -c "clear $DISP"
+setsid /bin/sh -c "echo \"$MSG\" $DISP"
+setsid /bin/sh -c "exec login $DISP"
+
+echo "..out of GETTY-$TTY!!!!!!!!!!!!!11111111"
