5e93c70594d6c5bab7a98489e707f1af7146be42
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-19 17:35:00 +0300

    Update xx40-copy-binary.sh

--- scripts/xx40-copy-binary.sh
+++ scripts/xx40-copy-binary.sh
@@ -3,9 +3,11 @@ set -e
 
 source .config
 
-
 echo "--> BINARIES.."
+
+echo "--> recopy BusyBox.."
 [ -d "$BINARY_BBDyn" ] && sudo cp -rv "$BINARY_BBDyn/"* "$NEWROOT/"
 
+echo "--> OTHERS bins.."
 [ -d "$BINARY_OTHERS" ] && sudo cp -rv "$BINARY_OTHERS/"* "$NEWROOT/"
 
