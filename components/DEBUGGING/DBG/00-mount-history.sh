8914ceb9f072917864484926d73bdf85dda07bdb
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-02-11 18:04:38 +0300

    Update 00-mount-history.sh

--- components/DEBUGGING/DBG/00-mount-history.sh
+++ components/DEBUGGING/DBG/00-mount-history.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-mount -t vboxsf exch /mnt/guest-exch
+# QEMU-style shared folder
+mount -t 9p guest-exch /mnt/guest-exch -o msize=10485760
+
+
+# VB-style shared folder
+#mount -t vboxsf exch /mnt/guest-exch
